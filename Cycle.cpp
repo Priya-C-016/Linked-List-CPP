eba53c035916d62d8b4501e71e3714229b9d51f1
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-05-02 13:47:38 +0530

    Cycle.cpp

---
+++ Cycle.cpp
@@ -0,0 +1,58 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* head=NULL;
+void insert(int data){
+    Node*newNode=new Node();
+    newNode->data=data;
+    newNode->next=NULL;
+    if(head==NULL){
+        head=newNode;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newNode;
+
+}
+
+bool cycle(Node* head){
+    Node* slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<"\n";
+}
+int main() {
+   int n;
+   cout<<"Enter the number";
+   cin>>n;
+   while(n--){
+       int data;cin>>data;
+       insert(data);
+   }
+   head->next->next = head;
+//   print(head);
+   cout<<cycle(head);
+   
+
+    return 0;
+}
